c2d0a2a89fc3c0a66620a23760573e8a71e32d3a
Dusan Janecek <dusanjanecek@dusans-macbook-pro.local>
2016-09-13 12:10:19 +0200

    Initial Commit

---
+++ SaveTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveTheWorld
+//
+//  Created by Dusan Janecek on 13/09/16.
+//  Copyright © 2016 Dusan Janecek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
